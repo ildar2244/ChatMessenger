44e5b2802781469409072e4fa66a71f77faacf2d
AkhmetshinIK <ildar2244@gmail.com>
2020-07-09 10:19:49 +0300

    Data layer. Added: editing user, working with file.

--- app/src/main/java/ru/axdar/chatmessenger/data/account/AccountRepositoryImpl.kt
+++ app/src/main/java/ru/axdar/chatmessenger/data/account/AccountRepositoryImpl.kt
@@ -14,6 +14,7 @@ class AccountRepositoryImpl(
         return accountCache.getToken().flatMap {
             accountRemote.login(email, password, it)
         }.onNext {
+            it.password = password
             accountCache.saveAccount(it)
         }
     }
@@ -49,6 +50,12 @@ class AccountRepositoryImpl(
     }
 
     override fun editAccount(entity: AccountEntity): Either<Failure, AccountEntity> {
-        throw UnsupportedOperationException("Editing account is not supported")
+        return accountCache.getCurrentAccount().flatMap {
+            accountRemote.editUser(entity.id, entity.email, entity.name, entity.password,
+                entity.status, it.token, entity.image)
+        }.onNext {
+            entity.image = it.image
+            accountCache.saveAccount(entity)
+        }
     }
 }
